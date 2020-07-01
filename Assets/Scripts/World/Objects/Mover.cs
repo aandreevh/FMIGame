155293ff1839cd72efdeec017e87031ac8f82745
hykar <aandreevh@gmail.com>
2020-07-01 17:43:28 +0300

    Added basic mover

---
+++ Assets/Scripts/World/Objects/Mover.cs
@@ -0,0 +1,25 @@
+using System;
+using Signals.Receivers.Unary;
+using UnityEngine;
+using World.Navigation;
+
+namespace World.Objects
+{
+    [RequireComponent(typeof(Agent))]
+    public class Mover : UnaryReceiver
+    {
+        
+        
+        private Agent Agent { get; set; }
+
+        private void Awake()
+        {
+            Agent = GetComponent<Agent>();
+        }
+
+        private void FixedUpdate()
+        {
+            if(Signal && Signal.Signaled) Agent.UpdateAgent();
+        }
+    }
+}
